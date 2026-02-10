caad374109a4213a1b11810ad39e627bc5d9c3fe
ikromjonxursanov <xursanovikromjon2@gmail.com>
2026-02-10 11:52:17 +0500

    added category model to products app

---
+++ apps/products/models/categories.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils.text import slugify
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
